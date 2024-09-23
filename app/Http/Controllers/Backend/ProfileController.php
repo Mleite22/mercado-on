6692c89cf8cf0757f5bd1e7c67de128e1ed116b4
Mleite22 <leitelopesmarcos@gmail.com>
2024-09-23 18:47:13 -0400

    Atualaização de senha

---
+++ app/Http/Controllers/Backend/ProfileController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use File;
+
+class ProfileController extends Controller
+{
+    //Ver perfil do ususario
+    public function index()
+    {
+        return view('admin/profile/index');
+    }
+
+    //Atualizar profile com foto
+    public function update(Request $request)
+    {
+        //Validação
+        // dd($request->all());
+        $request->validate([
+            'name' => ['required', 'max:100'],
+            'email' => ['required', 'email', 'unique:users,email,' . Auth::user()->id],
+            'image' => ['image', 'max:2048'],
+
+        ]);
+        //Atualizar
+        $user = Auth::user(); // Adicione uma dica de tipo
+
+        if($request->hasFile('image')){
+
+            //Se existe uma foto na pasta, deleta e substitui
+            if(File::exists(public_path( $user->image))){
+                File::delete(public_path( $user->image));
+
+            }
+
+            $image = $request->image;
+            $imageName = rand() . '-mldesigner-' . $image->getClientOriginalName();
+            $image->move(public_path('uploads'), $imageName);
+            //Caminho da pasta de imagens
+            $path = "/uploads/" . $imageName;
+            $user->image = $path;
+        }
+
+        /** @var \App\Models\User $user */
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
+        return redirect()->back()->with('success', 'Perfil atualizado com sucesso!');
+    }
+
+    public function updatePassword(Request $request)
+    {
+        //dd($request->all());
+        $request->validate([
+            'current_password' => ['required', 'current_password'],
+            'password' => ['required', 'min:8', 'confirmed'],
+        ]);
+        // Atualização
+        $request->user()->update([
+            'password' => bcrypt($request->password),
+        ]);
+        return redirect()->back()->with('successSenha', 'Senha atualizada com sucesso!');
+        
+    }
+}
--- routes/web/admin.php
+++ routes/web/admin.php
@@ -1,7 +1,24 @@
 <?php
 
-use App\Http\Controllers\Backend\AdminController; 
+use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 //rota admin
-Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->middleware(['auth', 'admin'])->name('admin.dashboard');
+Route::get('admin/dashboard', [AdminController::class, 'dashboard'])
+->middleware(['auth', 'admin'])
+->name('admin.dashboard');
+
+//Rota para ver o perfil admin
+Route::get('admin/profile', [ProfileController::class, 'index'])
+->middleware(['auth', 'admin'])
+->name('admin.profile');
+//Rota para atualizar o Usuario
+Route::post('admin/profile/update', [ProfileController::class, 'update'])
+->middleware(['auth', 'admin'])
+->name('admin.profile.uptade');
+
+//Rota para atualizar s Senha
+Route::post('admin/profile/update/password', [ProfileController::class, 'updatePassword'])
+->middleware(['auth', 'admin'])
+->name('admin.profile.password');
